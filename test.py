c05e1581a5c420d7973ca560137354da8eedbecd
Devasy23 <20bce057@nirmauni.ac.in>
2022-12-30 11:26:57 +0530

    asdf

--- test.py
+++ test.py
@@ -11,8 +11,9 @@ images = []
 # Initialize the count variable
 count = 0
 
-def face_extractor(img):
+def face_extractor(frame):
     # load the face cascade
+    img = frame.to_ndarray(format="bgr24")
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     # convert to grayscale
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -22,9 +23,11 @@ def face_extractor(img):
     if faces == ():
         return None
     # crop all faces found
+    st.write(type(faces))
     for (x,y,w,h) in faces:
         cropped_face = img[y:y+h, x:x+w]
-    return cropped_face
+    return av.VideoFrame.from_ndarray(cropped_face, format="bgr24")
+
 # Define a transformer class to process the video frames
 def callback(frame):
     img = frame.to_ndarray(format="bgr24")
