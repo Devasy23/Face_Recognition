8487180af1a70826d38b502f1fbb8362841f1ac1
Devasy23 <20bce057@nirmauni.ac.in>
2022-12-30 11:33:03 +0530

    wertc

--- test.py
+++ test.py
@@ -37,7 +37,9 @@ def callback(frame):
     return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 # Stream video from the user's webcam using the webrtc_streamer function
-webrtc_streamer(key="example", video_frame_callback=callback)
+webrtc_streamer(key="example", video_frame_callback=callback,rtc_configuration={  # Add this line
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+    })
 
 
 
